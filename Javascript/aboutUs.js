842efad3bfeaf7552615e8f99e3a1c1f4431a341
haneeshaseef <haneeshaseef521@gmail.com>
2023-07-22 10:17:36 +0530

    Completed student 03 tasks commit

--- Javascript/aboutUs.js
+++ Javascript/aboutUs.js
@@ -1,14 +1,23 @@
-document.addEventListener('DOMContentLoaded',() =>{
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all the images with the class "student-image"
+  let images = document.querySelectorAll("img");
 
-function displayStudnetDetails(imageId , studentId, Name){
-    console.log(imageId)
-    document.getElementById(imageId).style.opacity = '0.5'
-    document.getElementById(studentId).innerHTML = Name +",  Role : " + studentId
-}
+  //Loop through the images and add event listeners for mouseover and mouseout
+  images.forEach((image) => {
+    let studentId = image.getAttribute("data-student-id");
+    let roleId = image.getAttribute("data-role-id");
+    let name = image.getAttribute("data-name");
 
- function hideStudentDetails(imageId , studentId){
-    document.getElementById(imageId).style.opacity = '1'
-    document.getElementById(studentId).innerHTML = ''
- }
+    // Add a mouseover event listener that displays the student details
+    image.addEventListener("mouseover", () => {
+      document.getElementById(studentId).innerHTML = name;
+      document.getElementById(roleId).innerHTML = "Student Role : " + studentId;
+    });
+
+    // Add a mouseout event listener that hides the student details
+    image.addEventListener("mouseout", () => {
+      document.getElementById(studentId).innerHTML = "Student Name";
+      document.getElementById(roleId).innerHTML = "Student Role";
+    });
+  });
+});
--- Javascript/quiz.js
+++ Javascript/quiz.js
@@ -1,3 +1,4 @@
+//Javascript object which contains the questions for quiz
 const questions = {
   1: "Which country hosted the 2022 FIFA World Cup?",
   2: "Who is the youngest player to ever score in a World Cup tournament?",
@@ -11,6 +12,7 @@ const questions = {
   10: "What is the name of the official match ball for the 2022 World Cup?",
 };
 
+//Javascript object which contains the answers for the quiz
 const Answers = {
   1: {
     1: "Spain",
@@ -74,6 +76,7 @@ const Answers = {
   },
 };
 
+//Javascript object which contains the correct answers of the quiz
 const correctAnswers = {
   1: "Qatar",
   2: "Pele",
@@ -88,85 +91,136 @@ const correctAnswers = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Declare variables for quiz count, quiz left, wrong answer count and buttons
   let quizCount = 1;
   let quizleft = 10;
-  let correctAnswerCount = 0;
+  let wrongAnswerCount = 0;
   let buttons = document.querySelectorAll("button");
 
+  // Set a timer for the quiz of 10 seconds for each question
   let quizTimer = setInterval(() => {
+    // Display the quiz count out of 10
     document.getElementById("questioncount").innerHTML = quizCount + " /10";
+    // Set a variable for time left and display it
     let timeleft = 10;
-    let timer = setInterval(function () {
+    let timer = setInterval(() => {
       document.getElementById("time").innerHTML = timeleft;
+      // Decrease the time left by one every second
       timeleft--;
+      // If the time left is zero, clear the timer and reset the button styles
       if (timeleft == 0) {
         clearInterval(timer);
         document.getElementById("time").innerHTML = timeleft;
         buttons.forEach((button) => {
           button.style.backgroundColor = "";
-          button.style.color = "black"
+          button.style.color = "black";
         });
       }
     }, 1000);
 
+    // Display the question and the answers from the questions and Answers arrays
     document.getElementById("question").innerHTML =
       quizCount + ". " + questions[quizCount];
     for (j = 1; j <= 4; j++) {
       document.getElementById("answer" + j).innerHTML = Answers[quizCount][j];
     }
 
+    // Declare a variable to check if the user has clicked an answer
     let clicked = false;
-    
+
+    // Define a function to handle the click event on the buttons
     function handleClick(e) {
-        if (!clicked) {
-          clicked = true;
+      // If the user has not clicked an answer yet, set clicked to true
+      if (!clicked) {
+        clicked = true;
 
-          buttons.forEach((button)=>{
-            let getText = button.innerHTML
+        // Loop through all the buttons and check if they match the correct answers from the correctAnswers object
+        buttons.forEach((button) => {
+          let getText = button.innerHTML;
 
-            let keys = Object.keys(correctAnswers);
-            keys.forEach((key)=>{
-                if(getText === correctAnswers[key]){
-                    button.style.backgroundColor = "green";
-                    button.style.color = "white"
-                }
-            }) 
-          })
-      
-          let getAnswer = e.target.innerHTML;
-      
-          if (Object.values(correctAnswers).includes(getAnswer)) {
-            e.target.style.backgroundColor= "green";
-            correctAnswerCount++;
-          } else {
-            e.target.style.backgroundColor = "red";
-            e.target.style.color = "white";
-          }
+          let keys = Object.keys(correctAnswers);
+          keys.forEach((key) => {
+            // If the button matches a correct answer, change its background color to green and text color to white
+            if (getText === correctAnswers[key]) {
+              button.style.backgroundColor = "green";
+              button.style.color = "white";
+            }
+          });
+        });
+
+        // Get the answer from the clicked button
+        let getAnswer = e.target.innerHTML;
+
+        // If the answer is correct, change its background color to green
+        if (Object.values(correctAnswers).includes(getAnswer)) {
+          e.target.style.backgroundColor = "green";
+        } else {
+          // If the answer is wrong, change its background color to red, text color to white and increase the wrong answer count by one
+          e.target.style.backgroundColor = "red";
+          e.target.style.color = "white";
+          wrongAnswerCount++;
         }
+      }
     }
 
-    
+    // Add an event listener for each button to call the handleClick function when clicked
     buttons.forEach((button) => {
       button.addEventListener("click", handleClick);
     });
 
+    // Increase the quiz count by one and decrease the quiz left by one
     quizCount++;
     quizleft--;
+    // If there are no more quizzes left, clear the quiz timer and display a button to view the score after 10 seconds
     if (quizleft == 0) {
       clearInterval(quizTimer);
-      setTimeout(()=>{
-        document.getElementById('viewScore').innerHTML = "View my score"
-        document.getElementById('viewScore').classList.add("viewScore")
-      },10000)
-      
+      setTimeout(() => {
+        document.getElementById("viewScore").innerHTML = "View my score";
+        document.getElementById("viewScore").classList.add("viewScore");
+      }, 10000);
     }
   }, 10000);
 
-  function viewPopUp(){
-    console.log("hello")
+  // Add an event listener for the view score button to call the viewPopUp function when clicked
+  document.getElementById("viewScore").addEventListener("click", viewPopUp);
+
+  // Declare a variable for the quiz score view element
+  let quizScoreView = document.getElementById("quizScoreView");
+  // Define a function to display the quiz score view as a pop up
+  function viewPopUp() {
+    // Set the display style of the quiz score view to block
+    quizScoreView.style.display = "block";
+    // Display the wrong answer count
+    document.getElementById("correctAnswerCount").innerHTML =
+      "wrong Answers :" + wrongAnswerCount;
+
+    // Calculate and display the score as a percentage of correct answers out of 10
+    let correctAnswerCount = 10 - wrongAnswerCount;
+    document.getElementById("score").innerHTML =
+      "Score : " + (correctAnswerCount / 10) * 100;
+
+    // Declare a variable for the progress element and display a message based on the score
+    let progress = document.getElementById("progress");
+
+    if (correctAnswerCount >= 4) {
+      progress.innerHTML = "you are doing well. Keep it up!";
+      progress.style.color = "green";
+    } else {
+      progress.innerHTML = "Try your best!";
+      progress.style.color = "red";
+    }
   }
-  
-  document.getElementById("viewScore").addEventListener("click",viewPopUp)
+
+  // Declare a variable for the close button and add an event listener to close the pop up when clicked
+  let close = document.getElementById("closeBtn");
+  close.onclick = function () {
+    quizScoreView.style.display = "none";
+  };
+
+  // Add an event listener to close the pop up when the user clicks outside of it
+  window.onclick = function (event) {
+    if (event.target == quizScoreView) {
+      quizScoreView.style.display = "none";
+    }
+  };
 });
-
-
