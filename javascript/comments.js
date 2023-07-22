77db037cef9bf4ae8616273b0a6fe296777f9329
abd456 <abdhubasith456@gmail.com>
2023-07-22 20:02:48 +0530

    2023/07/22 20:02

--- javascript/comments.js
+++ javascript/comments.js
@@ -14,8 +14,3 @@ function thankYouMessage() {
     document.getElementById("myForm").style.display = "none";
     document.getElementById("ty").style.display="block";
 }
-
-// document.getElementById("notFilled").innerHTML= "<div class='questionBox' id='notFilledAlert'>&#9888; This question is required</div>";
-//             document.getElementById("notFilledAlert").style.cssText= "background-Color: red; color: white";
-//             document.getElementById("feedbackId").style.border="solid 1px red";
-//             return false;
