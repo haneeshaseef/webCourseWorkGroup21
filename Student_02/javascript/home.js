f2aa0b96f01b2106b0c5e90620f2445c4f1f90f7
abd456 <abdhubasith456@gmail.com>
2023-07-23 18:33:03 +0530

    studentwise updated

---
+++ Student_02/javascript/home.js
@@ -0,0 +1,15 @@
+function goToHistoryPage() {
+    window.location.href = "history.html";
+}
+
+function goToTeamsPage() {
+    window.location.href = "teams.html";
+}
+
+function goToFansPage() {
+    window.location.href = "fans.html";
+}
+
+function goToLegendsPage() {
+    window.location.href = "legends.html";
+}
