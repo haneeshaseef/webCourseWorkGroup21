11be03e74406ff66ac1ede7f6f5947744588edc7
haneeshaseef <haneeshaseef521@gmail.com>
2023-07-23 10:44:07 +0530

    completed website

---
+++ Student_01/javascript/shop.js
@@ -0,0 +1,72 @@
+const button1 = document.getElementById("check-total");
+const checkout = document.getElementById("checkout");
+
+function updateQuantity(inputId, tdId) {
+  const inputField = document.getElementById(inputId);
+  const td = document.getElementById(tdId);
+  const value = inputField.value;
+  td.textContent = value;
+}
+
+function updatePrice(itemPrice,quantityId,quantityPriceId){
+    const td =document.getElementById(quantityId)
+    const totalPriceId=document.getElementById(quantityPriceId)
+    const quantity=td.textContent
+    const price = itemPrice*quantity
+    totalPriceId.textContent=price;
+}
+let totalCost;
+
+function calcTotal(){
+  const boot =document.getElementById("boot-total").textContent;
+  const glove =document.getElementById("glove-total").textContent;
+  const ball = document.getElementById("ball-total").textContent;
+  const pad =document.getElementById("pad-total").textContent;
+  const bip =document.getElementById("bip-total").textContent;
+  const cone =document.getElementById("cone-total").textContent;
+  const jersey =document.getElementById("jersey-total").textContent;
+  const bottle =document.getElementById("bottle-total").textContent;
+  totalCost = parseInt(boot) + parseInt(glove) + parseFloat(ball) + parseInt(pad) + parseInt(bip) + parseFloat(cone) + parseInt(jersey) + parseInt(bottle);
+  const setTotal= document.getElementById("total-cost");
+  setTotal.textContent=totalCost+" $";
+}
+
+
+button1.addEventListener("click", function() {
+  updateQuantity("boot-input", "boot-quantity");
+  updatePrice(99.99,"boot-quantity","boot-total")
+  updateQuantity("glove-input","glove-quantity")
+  updatePrice(49.99,"glove-quantity","glove-total")
+  updateQuantity("ball-input","ball-quantity")
+  updatePrice(50.99,"ball-quantity","ball-total")
+  updateQuantity("pad-input","pad-quantity")
+  updatePrice(19.99,"pad-quantity","pad-total")
+  updateQuantity("bip-input","bip-quantity")
+  updatePrice(20.99,"bip-quantity","bip-total")
+  updateQuantity("cone-input","cone-quantity")
+  updatePrice(10.99,"cone-quantity","cone-total")
+  updateQuantity("jersey-input","jersey-quantity")
+  updatePrice(60.99,"jersey-quantity","jersey-total")
+  updateQuantity("bottle-input","bottle-quantity")
+  updatePrice(15.99,"bottle-quantity","bottle-total")
+  calcTotal();
+
+});
+
+let total = document.getElementById('total-cost')
+
+checkout.addEventListener("click",()=>{
+  if(totalCost!=0 && totalCost!= null){
+    window.location.href="Checkout.html";
+  }else{
+    document.getElementById('ALERT').innerHTML = "<div class='questionBox' id='notFilledAlert'>&#9888; Order something to proceed .</div>";
+    document.getElementById("notFilledAlert").style.cssText= "background-Color: red; color: white;  padding: 10px; border-radius: 4px;";
+  }
+  
+});
+
+
+
+
+
+
